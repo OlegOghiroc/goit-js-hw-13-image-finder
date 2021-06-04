33853bed808ad9374a717022d46d73e53e6c663d
OlegOghiroc <olegoghiroc@gmail.com>
2021-06-04 23:58:04 +0100

    added markap rendering

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import FetchIpi from './js/apiService';
+import imageCard from './thamplates/imageCard.hbs';
+const photosFetch = new FetchIpi();
 
-const REFS = {
+const refs = {
+    formInput: document.querySelector('.search-form'),
+    cardList: document.querySelector('.gallery')
+}
+refs.formInput.addEventListener('submit', onInputSearch)
 
+
+
+
+async function onInputSearch(e) {
+    e.preventDefault()
+    photosFetch.query = e.currentTarget.elements.query.value;
+    console.log(photosFetch.query)
+    const result = await photosFetch.getFetch(photosFetch.inputSearchValue)
+    renderMarkap(result)
 }
 
-const photosFetch = new FetchIpi();
-console.log(photosFetch)
-const name = 'flower';
-photosFetch.getFetch(name);
+function renderMarkap(hits) {
+    refs.cardList.insertAdjacentHTML('beforeend', imageCard(hits))
+}
+
 // 1 добавити фнк слухача
 // 2 рендерити розмітку
 // 3 load more
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,17 +1,28 @@
 
+import axios from 'axios';
+
 const BASE_URL = `https://pixabay.com/api/`;
 const apikey = `21924983-61906be40b003d99bb3c70dab`;
 
 export default class FetchIpi {
     constructor() {
         this.key = apikey;
+         this.inputSearchValue = '';
+        
     }
-    getFetch(name) {
-        const url =`${BASE_URL}?image_type=photo&orientation=horizontal&q=${name}&page=2&per_page=12&key=${apikey}`;
-        fetch(url).then((response) => {
-            return response.json()
-        }).then((data) => {
-            console.log(data)
-        })
+    async getFetch() {
+        const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.inputSearchValue}&page=2&per_page=12&key=${apikey}`;
+        const response = await axios.get(url);
+
+        console.log(response.data.hits)
+        return response.data.hits;
     }
+    get query() {
+        return this.inputSearchValue
+    }
+
+    set query(newQuery) {
+        this.inputSearchValue = newQuery  
+    }
+  
 }
